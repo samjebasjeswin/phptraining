f83ad96023c8749dac712439d67f28775abb5f82
sam <sam@gmail.com>
2025-10-16 20:25:17 +0530

    changes in config

--- config.php
+++ config.php
@@ -11,5 +11,6 @@ $db_name = $env['DB_NAME'];
 
     echo "$db_host, $db_user, $db_pass, $db_name";
     echo "$db_host, $db_user, $db_pass, $db_name";
+        echo "$db_host, $db_user, $db_pass, $db_name";
 
 ?>
